3c61a0168472d3e3c3c0bacacce3474ec386f4cd
Raphael <raphaelferraz5056@gmail.com>
2026-08-13 19:48:01 -0300

    Feat: Cria projeto Expo e explora estrutura com tela de boas-vindas

---
+++ praticas/pratica02/todo-basico/App.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+function App(){
+  return(
+    <View style={{margin: 100}}>
+      <Text>Programação para Dispositivos Móveis</Text>
+      <Text>Olá, Raphael!</Text>
+      <Text>Meu segundo passo com Expo e React Native</Text>
+    </View>
+  )
+}
+
+export default App;
