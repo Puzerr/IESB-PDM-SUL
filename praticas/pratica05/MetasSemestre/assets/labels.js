675b341126407fa7f4fea0c668c6a31736283420
Raphael <raphaelferraz5056@gmail.com>
2026-09-09 17:44:45 -0300

    feat: adiciona atividade 02

---
+++ praticas/pratica05/MetasSemestre/assets/labels.js
@@ -0,0 +1,5 @@
+const tituloHeader = "Minhas Metas"
+const placeholderNomeInserir = "Digite o nome da meta"
+const placeholderBotaoInserir = "Adicionar"
+
+export {tituloHeader, placeholderBotaoInserir, placeholderNomeInserir}
